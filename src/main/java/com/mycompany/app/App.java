acfb605bacc9b4edd116d97ec16a7f0c704f1347
root <root@ip-172-31-31-5.us-east-2.compute.internal>
2020-07-20 14:24:24 +0000

    7.54pm

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! time 7.53pm 20 july" );
     }
 }
