3a650e2f0f04c8d4025836516514839a4de19baa
Cherughattu Phani Kumar (FCA) <t7972pc@cag.cdroot.net>
2021-11-09 17:02:41 +0530

    changes

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! time 08.15pm 19 july 2021" );
+        System.out.println( "Hello team" );
     }
 }
